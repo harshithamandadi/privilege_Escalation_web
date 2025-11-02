46cebc0e3259aa5e4e2e063c32288680e16a8325
ashrayayelisetty <ashrayayelisetty@gmail.com>
2025-11-02 19:40:30 +0530

    changed index.js

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+// ...existing code...
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -5,6 +6,17 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
+
+// CORS: allow frontend origin(s) and allow cookies
+app.use(cors({
+  origin: ['https://privilege-escalation-web.onrender.com'], // include localhost while developing
+  credentials: true
+}));
+
+// allow preflight for all routes
+app.options('*', cors());
+
+// middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static('app/public'));
@@ -14,3 +26,4 @@ app.use('/', routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('TinyTasks (vuln) running on port', PORT));
+// ...existing code...
