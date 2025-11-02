1d60ae61449c74cf975d36a90984010019896199
ashrayayelisetty <ashrayayelisetty@gmail.com>
2025-11-02 19:20:48 +0530

    origin url added

--- app/index.js
+++ app/index.js
@@ -4,6 +4,23 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
 const app = express();
+
+const cors = require('cors');
+
+
+// Allow only your frontend's Render URL
+app.use(cors({
+  origin: 'https://privilege-escalation-web.onrender.com/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+
+
+// allow preflight for all routes
+app.options('*', cors());
+
+// middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static('app/public'));
